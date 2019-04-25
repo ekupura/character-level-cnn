4376ae5d2732f3567963aeffdcc6ffa890464839
ekupura <ekupura9@gmail.com>
2019-04-25 18:30:12 +0900

    add function to convert sentences to one-hot matrices

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.utils import np_utils
 import pandas as pd
 import numpy as np
 import codecs
 import re
+from tqdm import tqdm
 
 
 def common_preprocess_sentiment140(input_name):
@@ -25,7 +27,7 @@ def common_preprocess_sentiment140(input_name):
 def character_restriction(dataset, restriction_rule=r'[^\w!?,\s]'):
     labels, texts = [], []
     for l, t in zip(dataset["label"], dataset["text"]):
-        re_text = re.sub(restriction_rule, '', t).strip()
+        re_text = re.sub(r'\s', ' ', re.sub(restriction_rule, '', t)).strip()
         if not re_text:
             continue
         texts.append(re_text)
@@ -35,14 +37,45 @@ def character_restriction(dataset, restriction_rule=r'[^\w!?,\s]'):
     return pd.DataFrame({'text': texts, 'label': labels})
 
 
+def conversion_rule_for_alphabet(sentence):
+    id_list = []
+    for c in sentence:
+        if c.islower():
+            id_list.append(ord(c) - 97)
+        elif c.isupper():
+            id_list.append(ord(c) - 39)
+        elif c.isdecimal():
+            id_list.append(int(c) + 52)
+        elif c == '?':
+            id_list.append(62)
+        elif c == '!':
+            id_list.append(63)
+        elif c == ',':
+            id_list.append(64)
+        else:
+            id_list.append(65)
+    return id_list
+
+
+def character_to_onehot(sentences, conversion_rule=conversion_rule_for_alphabet):
+    characters_id_list = [[conversion_rule(c) for c in t] for t in tqdm(sentences)]
+    sentence_onehot_matrix = np_utils.to_categorical(characters_id_list)
+    print(sentence_onehot_matrix.shape)
+    return sentence_onehot_matrix
+
+
+def label_to_onehot(labels):
+    pass
+
 def train():
     model = Sequential()
 
 
 def main():
-    dataset = common_preprocess_sentiment140('dataset/sentiment140.csv')
-    dataset2 = character_restriction(dataset, restriction_rule=r'[^\w!?,\s]')
-    print(dataset2)
+    extracted_dataset = common_preprocess_sentiment140('dataset/sentiment140.csv')
+    #[0-9a-zA-Z_!?,space] 66 characters
+    restricted_dataset = character_restriction(extracted_dataset, restriction_rule=r'[^\w!?,\s]')
+    text_matrices = character_to_onehot(restricted_dataset['text'], conversion_rule_for_alphabet)
 
 
 if __name__ == '__main__':
