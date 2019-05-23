42b7b9e468bb8600cd1048053d2f7bbd4aa8d479
ekupura <ekupura9@gmail.com>
2019-05-24 02:28:59 +0900

    fix some functions and parameters

--- simplecnn.py
+++ simplecnn.py
@@ -1,5 +1,5 @@
 from keras.models import Model, load_model
-from keras.layers import Input, Dense, Conv2D, Dropout, MaxPooling2D, Lambda, Embedding
+from keras.layers import Input, Dense, Conv2D, Dropout, MaxPooling2D, Lambda, Embedding, Reshape
 from keras.optimizers import SGD
 from keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
 import keras.backend as K
@@ -8,24 +8,25 @@ import tensorflow as tf
 import keras.backend.tensorflow_backend as ktf
 import numpy as np
 
-batch_size = 128
-embedding_dimension = 50
-filter_size = 256
-n_folds = 3
-epochs = 5
+n_folds = 1
+epochs = 1
 
 
 def configure(limit_characters, number_of_characters):
+    # parameter
+    embedding_dimension = 50
+    filter_size = 256
+    # layer
     inputs = Input(shape=(limit_characters, ))
     x1 = Embedding(input_dim=number_of_characters, output_dim=embedding_dimension,
                    embeddings_initializer='uniform', mask_zero=False)(inputs)
-    x2 = Lambda(lambda a: K.reshape(a, (batch_size, limit_characters, embedding_dimension, 1)))(x1)
+    x2 = Reshape(target_shape=(limit_characters, embedding_dimension, 1))(x1)
     x3 = Conv2D(filters=filter_size, kernel_size=(3, embedding_dimension), padding='valid',
-                activation='relu', input_shape=(batch_size, limit_characters, embedding_dimension, 1),
+                activation='relu',
                 data_format='channels_last')(x2)
     x4 = MaxPooling2D((limit_characters-2, 1), padding='valid')(x3)
     x5 = Dropout(0.5)(x4)
-    x6 = Lambda(lambda b: K.reshape(b, (batch_size, filter_size, )))(x5)
+    x6 = Reshape(target_shape=(filter_size, ))(x5)
     x7 = Dense(512, activation='relu')(x6)
     x8 = Dense(512, activation='relu')(x7)
     prediction = Dense(2, activation='softmax')(x8)
@@ -43,13 +44,15 @@ def callbacks():
 def train(x, y, limit_characters, number_of_characters):
     session = tf.Session('')
     ktf.set_session(session)
-    ktf.set_learning_phase(1)
     for i in range(n_folds):
         print("Training on Fold: ", i + 1)
         result = fit_and_evaluate(x, y, limit_characters, number_of_characters)
 
 
 def fit_and_evaluate(x, y, limit_characters, number_of_characters):
+    # parameter
+    batch_size = 128
+    # generate model
     model = configure(limit_characters, number_of_characters)
     for layer in model.layers:
         layer.trainable = True
@@ -70,5 +73,7 @@ def fit_and_evaluate(x, y, limit_characters, number_of_characters):
 
 
 def test(x, y):
+    batch_size = 128
     model = load_model('./model/fast_charcnn.h5')
+    x, y = x[x.shape[0] % batch_size:], y[y.shape[0] % batch_size:]
     print("Test Score: ", model.evaluate(x, y, batch_size=batch_size))
